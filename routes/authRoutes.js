894b4042c3e7f7908d48a6afeda7d1d6a6efed8a
Matthew <madd1778@yahoo.com>
2018-07-27 14:24:45 -0700

    added cookie session to passport. able to serialize a cookie for all users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
   app.get(
     '/auth/google', 
     passport.authenticate('google',{
@@ -9,6 +11,8 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 
-}
+};
