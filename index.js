a16cd019f1bec04bda4da370edaa6dac3c0e55c1
Matthew <madd1778@yahoo.com>
2018-07-26 17:22:37 -0700

    added mongoose module. Created a model folder with a 'users' file in database

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+require('./services/passport');
+require('./models/user');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
