d97ba0195e657adba889eee0716dac343ee612cc
Matthew <madd1778@yahoo.com>
2018-07-30 12:41:49 -0700

    removed proxy

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      proxy: true //testing proxy
+
     }, 
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
