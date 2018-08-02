795838473d4fcba4d2eef22aaee95a53f54a8e69
Matthew <madd1778@yahoo.com>
2018-08-02 12:36:42 -0700

    adding dev and prod stripe keys on server side

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,8 @@ module.exports = {
   googleClientID: "839293607766-q8emq67tvvhr5ehqh8g6no53pk7ak890.apps.googleusercontent.com",
   googleClientSecret: "Io0XP_gWvW5j4Z680Y9vNExw",
   mongoURI: "mongodb://tigerbombz:madd1778@ds153851.mlab.com:53851/my-2-cents-dev",
-  cookieKey: "lkjfjfsldknfsdjlkfjasld"
+  cookieKey: "lkjfjfsldknfsdjlkfjasld",
+  stripePublishableKey: "pk_test_HcQpBMvZRNDvo5CleSbBuFEb",
+  stripeSecretKey: "sk_test_6fZg6xsXp0uH80QgVWoiltSH"
 };
 
