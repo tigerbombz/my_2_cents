4a8fb6030af703926b96f19cd8a7bae038703bf3
Matthew <madd1778@yahoo.com>
2018-08-02 11:45:11 -0700

    trying to fix merge conflicts

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,13 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   app.get(
     '/auth/google', 
@@ -13,7 +19,7 @@ module.exports = (app) => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 
   app.get('/api/current_user', (req, res) => {
